2e1c77b1e01f60a9ce7584fd72829448ce3aa1d0
Abdusamatova Diyora <94184854+Diyora05@users.noreply.github.com>
2021-11-12 19:19:16 +0300

    02-dars qo'shildi
    yangilandi

---
+++ 02-shakllar.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Nov 12 17:46:52 2021
+
+@author: Demo
+"""
+print('*' * 12)
+print('*' * 4 + ' ' * 4 + '*' * 4)
+print('*' * 1 + ' ' * 10 + '*' * 1)
+print('*' * 4 + ' ' * 4 + '*' *4)
+print('*' * 12)
